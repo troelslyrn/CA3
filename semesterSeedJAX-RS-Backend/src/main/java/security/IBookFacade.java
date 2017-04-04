32ddf1e68c6ee320abf12dd556afc63d8daed272
PeterBoss <peter2137@gmail.com>
2017-04-04 11:06:35 +0200

    Book facade

---
+++ semesterSeedJAX-RS-Backend/src/main/java/security/IBookFacade.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package security;
+
+import java.awt.print.Book;
+import java.util.List;
+
+/**
+ *
+ * @author Peter
+ */
+public interface IBookFacade {
+    
+    Book createBook(Book book);
+    Book getBookById(int id);
+    List<Book> getBooks();
+    Book editBook(Book book);
+    Book deleteBook(Book book);
+}
