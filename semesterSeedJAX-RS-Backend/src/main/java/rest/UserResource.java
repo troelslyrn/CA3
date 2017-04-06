94b398f7f72ed8e9eaea05a7e0b4b2f5ddfcf32b
mahnazzzz <mahnaazi@yahoo.com>
2017-04-06 10:08:59 +0200

    rolle i userResource bliver rettet

--- semesterSeedJAX-RS-Backend/src/main/java/rest/UserResource.java
+++ semesterSeedJAX-RS-Backend/src/main/java/rest/UserResource.java
@@ -26,48 +26,53 @@ import security.PasswordStorage;
 @Path("demouser")
 //@RolesAllowed("UserResource")
 public class UserResource {
-     @Context
+
+    @Context
     private UriInfo context;
 
     UserFacade fac = new UserFacade(Persistence.createEntityManagerFactory("pu_development", null));
     Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
     public UserResource() {
-      
+
     }
-  
-    
-  @GET
-  @Path("complete")
-  @Produces(MediaType.APPLICATION_JSON)
-  public Response getAllUser(){
-     List<IUser> users = fac.getAllUsers();
-     JsonArray jar = new JsonArray();
-      for (IUser user : users) {
-          JsonObject job = new JsonObject();
-          job.addProperty("username", user.getUserName());
-          JsonArray roles = new JsonArray();
-          job.add("roles", roles);
-          for (String role : user.getRolesAsStrings()) {
-              roles.add(role);
-          }
-          jar.add(job);
-      }
+
+    @GET
+    @Path("complete")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllUser() {
+        List<IUser> users = fac.getAllUsers();
+        JsonArray jar = new JsonArray();
+        for (IUser user : users) {
+            JsonObject job = new JsonObject();
+            job.addProperty("username", user.getUserName());
+            JsonArray roles = new JsonArray();
+
+            job.add("roles", roles);
+            List<String> r = user.getRolesAsStrings();
+            if (r != null) {
+                for (String role : user.getRolesAsStrings()) {
+                    roles.add(role);
+                }
+            }
+            jar.add(job);
+        }
         return Response.status(Response.Status.OK).entity(gson.toJson(jar)).build();
-  }
-  @POST
+    }
+
+    @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response createUser(String userJSON) {
         User user = gson.fromJson(userJSON, User.class);
-         try {
-             String password = PasswordStorage.createHash(user.getPassword());
-             user.setPassword(password);
-         } catch (PasswordStorage.CannotPerformOperationException ex) {
-             Logger.getLogger(UserResource.class.getName()).log(Level.SEVERE, null, ex);
-         }
+        try {
+            String password = PasswordStorage.createHash(user.getPassword());
+            user.setPassword(password);
+        } catch (PasswordStorage.CannotPerformOperationException ex) {
+            Logger.getLogger(UserResource.class.getName()).log(Level.SEVERE, null, ex);
+        }
         User u = fac.createUser(user);
-       return Response.status(Response.Status.OK).entity(gson.toJson(u)).build();
+        return Response.status(Response.Status.OK).entity(gson.toJson(u)).build();
         //return Response.status(Response.Status.OK).entity(userJSON).build();
     }
 }
