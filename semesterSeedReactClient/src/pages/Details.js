ed773ddd83c8b023720810a7b525f567bbd9bb4d
PeterBoss <peter2137@gmail.com>
2017-04-07 11:34:00 +0200

    delete button links to products now

--- semesterSeedReactClient/src/pages/Details.js
+++ semesterSeedReactClient/src/pages/Details.js
@@ -18,7 +18,9 @@ export default class Details extends React.Component {
                 <h3 style={{color: "steelblue"}}>Detailed info for the title: {book.title}</h3>
                 <h4> {book.info}</h4>
                 <h4>{book.moreInfo}</h4>
-                <button onClick={this.deleteBook.bind(this, id)}>Delete</button>
+                <Link to="products">
+                    <button onClick={this.deleteBook.bind(this, book.id)}>Delete</button>
+                </Link>
                 <br />
                 <Link to="/products">Products</Link>
             </div>
